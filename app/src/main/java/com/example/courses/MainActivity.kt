8caa8e925fea7bdea05419faf3cec2d756e3f12e
bigshrey <156182283+bigshrey@users.noreply.github.com>
2024-03-28 19:02:26 +1100

    grid implementation + datasource and strings updated to include all photos

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -6,33 +6,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-
-
-import androidx.compose.ui.Modifier
-
-
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
+import com.example.courses.data.DataSource
 import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
 
@@ -41,7 +37,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CoursesTheme {
-
+                TopicApp()
             }
         }
     }
@@ -55,18 +51,36 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = false)
 @Composable
-fun GreetingPreview(modifier: Modifier = Modifier) {
-    TopicGridItem()
+fun TopicApp() {
+    TopicGrid(DataSource.topics)
 }
 
 @Composable
-fun TopicGridItem(modifier: Modifier = Modifier) {
-    Card {
+fun TopicGrid(topicList: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 8.dp)
+    ) {
+        items(topicList) {topic ->
+            TopicGridItem(topic)
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun GreetingPreview(modifier: Modifier = Modifier) {
+    TopicApp()
+}
+
+@Composable
+fun TopicGridItem(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = Modifier) {
         Row {
             Box {
-                Image(painter = painterResource(id = R.drawable.photography),
+                Image(painter = painterResource(id = topic.imageResourceId),
                     contentDescription = null,
                     modifier = Modifier
                         .size(68.dp, 68.dp)
@@ -75,7 +89,7 @@ fun TopicGridItem(modifier: Modifier = Modifier) {
                 )
             }
             Column(modifier = Modifier) {
-                Text(text = stringResource(id = R.string.photography),
+                Text(text = stringResource(id = topic.stringResourceId),
                     style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(
                         start = 16.dp,
@@ -86,7 +100,7 @@ fun TopicGridItem(modifier: Modifier = Modifier) {
                 )
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Icon(painter = painterResource(id = R.drawable.ic_grain), contentDescription = null, modifier = modifier.padding(start = 16.dp))
-                    Text(text = "321", modifier = modifier.padding(start = 8.dp), style = MaterialTheme.typography.labelMedium)
+                    Text(text = topic.someNumber.toString(), modifier = modifier.padding(start = 8.dp), style = MaterialTheme.typography.labelMedium)
                 }
             }
         }
